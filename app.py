8442bab76dbbc6ff02fb760124bae208ee976302
aryanrao4689 <bcaaryan4689@gmail.com>
2026-02-03 12:34:44 +0530

    The web is ready

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Load the model
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
-
+scalar = pickle.load(open('scaling.pkl', 'rb'))
 # Optional: load scaler if needed
 # scalar = pickle.load(open('scaler.pkl', 'rb'))
 
@@ -29,6 +29,13 @@ def predict_api():
     output = regmodel.predict(new_data)
     print("Prediction:", output[0])
     return jsonify(output[0])
+@app.route('/predict' ,methods=["POST"])
+def predict():
+    data=[float(x)for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The house price prediction is{}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
